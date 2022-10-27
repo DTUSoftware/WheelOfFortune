3114ce5d99edd093129ee31d2f44c25d2b5f42f8
HeroGamers <hero@herogamers.dev>
2022-10-28 00:31:11 +0200

    Initial commit, basically done with game guessing logic, needs wheel/points logic

--- app/src/main/java/dk/dtu/s215827/wheeloffortune/MainActivity.kt
+++ app/src/main/java/dk/dtu/s215827/wheeloffortune/MainActivity.kt
@@ -3,18 +3,20 @@ package dk.dtu.s215827.wheeloffortune
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import dk.dtu.s215827.wheeloffortune.ui.theme.WheelOfFortuneTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val viewModel = PlayerViewModel()
         setContent {
             WheelOfFortuneTheme {
                 // A surface container using the 'background' color from the theme
@@ -22,22 +24,102 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    WheelOfFortune(viewModel)
                 }
             }
         }
     }
 }
 
+sealed class WheelResult(var label: String, var type: Int, var points: Int) {
+    object ThousandPoints : WheelResult("1000", 0, 1000)
+}
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Word(word: String, revealedCharArray: List<Char>) {
+    Text(text = word)
+
+    LazyRow() {
+        items(word.length) {
+            val char = word[it]
+            Text(text = if (char == ' ' || revealedCharArray.contains(char)) char.toString() else "_")
+        }
+    }
+}
+
+@Composable
+fun Wheel(onSpin: (WheelResult) -> Unit) {
+
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun WheelOfFortune(viewModel: PlayerViewModel) {
+    val currentWord by viewModel.currentWord.collectAsState()
+    val currentCategory by viewModel.currentCategory.collectAsState()
+    val revealedChars by viewModel.revealedLetters.collectAsState()
+    val status by viewModel.status.collectAsState()
+
+    var guess by remember { mutableStateOf("") }
+
+    Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+        Text(text = "Wheel of Fortune")
+        Text(text = revealedChars.toString())
+
+        when (status) {
+            2 -> {
+                Text(text = "GAME WON!!!")
+            }
+            3 -> {
+                Text(text = "GAME LOST...")
+            }
+            4 -> {
+                Text(text = "Wow, you completed them all!")
+            }
+        }
+        
+        Column(modifier = Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally, verticalArrangement = Arrangement.Center) {
+            Word(currentWord, revealedChars)
+            Wheel{
+
+            }
+
+            if (currentCategory.isNotEmpty()) {
+                Text(text = currentCategory)
+            }
+
+            if (status == 1) {
+                TextField(value = guess, onValueChange = {
+                    // Match for letters
+                    if (it.matches(Regex("[a-zA-z\\s]*"))) {
+                        guess = it.take(1).uppercase()
+                    }
+                })
+                Button(onClick = {
+                    viewModel.guessChar(guess[0])
+                    guess = ""
+                }) {
+                    Text(text = "Guess")
+                }
+            }
+            else if (status != 4) {
+                Button(onClick = { viewModel.newGame() }) {
+                    Text(text = if (status != 0) "Play Again" else "Play")
+                }
+            }
+
+            Button(onClick = { viewModel.newGame() }) {
+                Text(text = "Debug")
+            }
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
+    val viewModel = PlayerViewModel()
     WheelOfFortuneTheme {
-        Greeting("Android")
+        WheelOfFortune(viewModel)
     }
 }
---
+++ app/src/main/java/dk/dtu/s215827/wheeloffortune/PlayerViewModel.kt
@@ -0,0 +1,158 @@
+package dk.dtu.s215827.wheeloffortune
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import org.json.JSONObject
+import kotlin.random.Random
+
+class PlayerViewModel : ViewModel() {
+    val currentWord = MutableStateFlow("")
+    val currentCategory = MutableStateFlow("")
+    val currentPossibleEarning = MutableStateFlow(0)
+    val revealedLetters = MutableStateFlow(emptyList<Char>())
+    val lives = MutableStateFlow(0)
+    val points = MutableStateFlow(0)
+    val status = MutableStateFlow(0)
+
+    val alreadyPlayedWords = MutableStateFlow(emptyList<String>())
+
+    val randomSeed = Random(System.currentTimeMillis())
+
+    val wordFile = this::class.java.classLoader?.getResource("words.json")?.readText()
+    val categories = wordFile?.let { JSONObject(it) }
+    var wordsTotal = 0
+
+    init {
+        categories?.keys()?.forEach { key -> wordsTotal += categories.getJSONArray(key).length() }
+    }
+
+    fun newGame() {
+        if (categories != null) {
+            setLives(5)
+            setPoints(0)
+            newWord()
+            setPlaying()
+        }
+        else {
+            this.status.value = -1
+        }
+    }
+
+    fun setNotPlaying() {
+        this.status.value = 0
+    }
+
+    fun setPlaying() {
+        this.status.value = 1
+    }
+
+    fun setWon() {
+        this.status.value = 2
+        checkDone()
+    }
+
+    fun setLost() {
+        this.status.value = 3
+        checkDone()
+    }
+
+    fun setDone() {
+        this.status.value = 4
+    }
+
+    fun checkDone(): Boolean {
+        if (this.alreadyPlayedWords.value.size == wordsTotal) {
+            setDone()
+            return true
+        }
+        return false
+    }
+
+    fun newWord() {
+        if (!checkDone()) {
+            this.revealedLetters.value = emptyList()
+            val category = categories?.keys()?.asSequence()?.elementAt((0 until categories.length()).random(randomSeed))
+            if (category != null) {
+                val words = categories?.getJSONArray(category)
+
+                if (words != null) {
+                    var word = ""
+
+                    // TODO: this can loop forever, ohno
+                    while (word.isEmpty() || this.alreadyPlayedWords.value.contains(word)) {
+                        word = (words.get((0 until words.length()).random(randomSeed)) as String).uppercase()
+                    }
+
+                    this.currentWord.value = word
+                    this.currentCategory.value = category
+                    this.alreadyPlayedWords.value = this.alreadyPlayedWords.value.plus(this.currentWord.value)
+                }
+            }
+        }
+    }
+
+    fun guessChar(char: Char) {
+        if (isCharInWord(char) && !isRevealed(char)) {
+            revealChar(char)
+            addPoints(currentPossibleEarning.value)
+            setPossibleEarnings(0)
+
+            if (revealedLetters.value.size == currentWord.value.replace(" ", "").toList().distinct().size) {
+                setWon()
+            }
+        }
+        else {
+            subtractLives(1)
+        }
+    }
+
+    fun isCharInWord(char: Char): Boolean {
+        // ignoreCase technically not needed now
+        return this.currentWord.value.contains(char, ignoreCase = true)
+    }
+
+    fun isRevealed(char: Char): Boolean {
+        return this.revealedLetters.value.contains(char)
+    }
+
+    fun revealChar(char: Char) {
+        this.revealedLetters.value = this.revealedLetters.value.plus(char)
+    }
+
+    fun addPoints(points: Int) {
+        this.points.value += points
+    }
+
+    fun subtractPoints(points: Int) {
+        this.points.value -= points
+    }
+
+    fun setPoints(points: Int) {
+        this.points.value = points
+    }
+
+    fun setPossibleEarnings(points: Int) {
+        this.currentPossibleEarning.value = points
+    }
+
+    fun doBankruptcy() {
+        this.points.value = 0
+        setLost()
+    }
+
+    fun addLives(lives: Int) {
+        this.lives.value += lives
+    }
+
+    fun subtractLives(lives: Int) {
+        this.lives.value -= lives
+        if (this.lives.value <= 0) {
+            setLost()
+        }
+    }
+
+    fun setLives(lives: Int) {
+        this.lives.value = lives
+    }
+
+}
