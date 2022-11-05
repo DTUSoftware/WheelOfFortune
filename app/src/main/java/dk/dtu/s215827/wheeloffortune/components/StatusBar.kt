3c5eee3fdaa84fdadd4746a0d85cd4dc9f686ea2
HeroGamers <hero@herogamers.dev>
2022-11-05 22:18:50 +0100

    Make status bar pretty

--- app/src/main/java/dk/dtu/s215827/wheeloffortune/components/StatusBar.kt
+++ app/src/main/java/dk/dtu/s215827/wheeloffortune/components/StatusBar.kt
@@ -1,11 +1,21 @@
 package dk.dtu.s215827.wheeloffortune.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import dk.dtu.s215827.wheeloffortune.GameStatus
 import dk.dtu.s215827.wheeloffortune.PlayerViewModel
+import dk.dtu.s215827.wheeloffortune.R
 
 @Composable
 fun StatusBar(viewModel: PlayerViewModel) {
@@ -14,7 +24,19 @@ fun StatusBar(viewModel: PlayerViewModel) {
     val points by viewModel.points.collectAsState()
     val possibleEarnings by viewModel.currentPossibleEarning.collectAsState()
 
-    Text(text = "$lives lives | $points$")
+    Row(modifier = Modifier
+        .fillMaxWidth()
+        .padding(15.dp), horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.CenterVertically) {
+        Row() {
+            for (i in 1..lives) {
+                Icon(painter = painterResource(id = R.drawable.baseline_heart_24), contentDescription = "Full Heart")
+            }
+            for (i in 1..5-lives) {
+                Icon(painter = painterResource(id = R.drawable.baseline_heart_border_24), contentDescription = "Empty Heart")
+            }
+        }
+        Text(text = "$points$")
+    }
 
     when (status) {
         GameStatus.PLAYING -> {
